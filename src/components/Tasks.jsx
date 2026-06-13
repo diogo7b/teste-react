c4eb833fa8e4068bec3e72fb5970965e2de0b1f4
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2026-06-13 14:53:10 -0300

    modificando codigo não utilizado e adicionando dependencias

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,14 @@
+import { ChevronRightIcon } from "lucide-react";
+
 export default function Tasks(props) {
     return (
-        <ul className="space-y-4 p-6 bg-slate-200 rounded-md">{props.tasks.map((task) =>
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md ">{props.tasks.map((task) =>
             <li
                 key={task.id}
-                className="flex">
-                <button className="w-100 bg-slate-400 text-white p-2 rounded-md">{task.title}</button>
-                <button>
-                    Ver detalhes
+                className="flex gap-2">
+                <button className="text-left w-full bg-slate-400 text-white p-2 rounded-md ">{task.title}</button>
+                <button className="bg-slate-400 p-2 text-white rounded-md">
+                    <ChevronRightIcon />
                 </button>
             </li>
         )}</ul>
